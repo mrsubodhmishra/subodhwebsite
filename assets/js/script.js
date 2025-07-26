261002b5b84eaec87ee64d7d855a395196e556c9
mrsubodhmishra <89600019+mrsubodhmishra@users.noreply.github.com>
2025-07-26 17:40:26 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Accountant","Photo Blogger", "traveller and Trekking", "Photograper","Android Developer"],
+    strings: ["Accountant","Photo Blogger", "traveller and Trekking", "Photographer","Android Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
