5e750d840139ef75938ef5673b4a46f59c249792
mrsubodhmishra <89600019+mrsubodhmishra@users.noreply.github.com>
2025-07-28 18:48:42 +0545

    Update 404.js

--- assets/js/404.js
+++ assets/js/404.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     });
 });
 
-// disable developer mode
+// disable developer mode hahah
 document.onkeydown = function(e) {
   if(e.keyCode == 123) {
      return false;
